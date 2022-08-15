b037259e93afa0270814487e994eaedf0a80dadc
hsynas <hsynas@gmail.com>
2022-08-15 15:36:14 +0200

    first commit

---
+++ hsyn.js
@@ -0,0 +1,27 @@
+let mymane =prompt("adın ne gardaş");
+
+
+ document.querySelector("#myName").innerHTML = mymane
+
+ function showTime() {
+    const today = new Date();
+    let weekday = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let d = weekday[today.getDay()];
+    // Sunday - Saturday : 0 - 6
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s + " " + d;
+    setTimeout(showTime, 1000);
+    }
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+    }
+showTime()
+
+
+
